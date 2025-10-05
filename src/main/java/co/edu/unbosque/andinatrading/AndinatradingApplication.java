f0630e94b6f246889d5ccf01924e5f29b2243037
P <paula.guerrero.pineda@gmail.com>
2025-10-04 20:46:06 -0500

    Creación del proyecto base

---
+++ src/main/java/co/edu/unbosque/andinatrading/AndinatradingApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.andinatrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndinatradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndinatradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unbosque/andinatrading/AndinatradingApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.andinatrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndinatradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
